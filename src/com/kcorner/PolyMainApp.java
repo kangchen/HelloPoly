92e3d9777df816b77fbf60408c5bfd85cfa8c29d
Kang Chen <kang_chen@msn.com>
2012-12-31 14:55:08 -0800

    initial commit

---
+++ src/com/kcorner/PolyMainApp.java
@@ -0,0 +1,36 @@
+package com.kcorner;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+
+
+public class PolyMainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            EventQueue.invokeLater(new Runnable() {
+            	public void run() {
+            		try {
+            			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+            			PolygonView myPolygons = new PolygonView();                    
+            			Toolkit kit = myPolygons.getToolkit();
+            			Dimension d = kit.getScreenSize();
+            			myPolygons.setLocation((int) (d.width-myPolygons.getWidth()) / 2, (int) (d.height-myPolygons.getHeight() ) / 2);
+				
+            			myPolygons.setVisible(true);
+            			myPolygons.setResizable(false);
+            			myPolygons.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            		} catch(Exception e) {
+            			e.printStackTrace();
+            		}
+            	}
+            });
+	}
+
+}
