50a715e93ebcec767a493531828629d253757e36
Kang Chen <kang_chen@msn.com>
2012-12-31 19:07:01 -0800

    cleaning up some codes

--- src/com/kcorner/polygon/Polygons.java
+++ src/com/kcorner/polygon/Polygons.java
@@ -102,14 +102,6 @@ public class Polygons {
 		return angleInRadians;
 	}
 	
-//	public void drawPolygon() {
-//
-//	    Polygon p = new Polygon();
-//	    for (int i = 0; i < 5; i++) {
-//	      p.addPoint((int) (100 + 50 * Math.cos(i * 2 * Math.PI / 5)),
-//	          (int) (100 + 50 * Math.sin(i * 2 * Math.PI / 5)));
-//	    }
-//	}
 	
 	public String toString() {
 		String str;
